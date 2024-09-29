d490cca1f9e3e2d18589962d1be303d8d34ff9e7
Pradip Ghadge <121886588+GhadgePradip9971@users.noreply.github.com>
2024-09-29 22:06:38 +0530

    completed realtimeDi

--- IOC_Prroj_1-RealtimeDI_customer/src/main/java/com/nt/controller/MainController.java
+++ IOC_Prroj_1-RealtimeDI_customer/src/main/java/com/nt/controller/MainController.java
@@ -1,23 +1,28 @@
 package com.nt.controller;
 
+import com.nt.dto.CustomerDTO;
 import com.nt.service.ICustomerMgmtService;
 import com.nt.vo.CustomerVO;
 
-public class MainController<ICustomerMgmtSrvicce> {
-	private ICustomerMgmtService service;
-	
-	
-	public MainController(ICustomerMgmtSrvicce service) {
-		System.out.println("MainController:1-param constructor");
-		
-	}
-	
-	public String processCustomer(CustomerVO vo) {
-	//convert vo class obj data to dto clas object data
-		
-		
-		
-		
-	}
+public class MainController {
+    private ICustomerMgmtService service;
 
+    public MainController(ICustomerMgmtService service) {
+        System.out.println("MainController: 1-param constructor");
+        this.service = service; // Assign the service to the instance variable
+    }
+
+    public String processCustomer(CustomerVO vo) throws Exception {
+        // Convert VO class object data to DTO class object data
+        CustomerDTO dto = new CustomerDTO();
+        dto.setCustName(vo.getCustName());
+        dto.setCustAddrs(vo.getCustAddrs());
+        dto.setPamt(Float.parseFloat(vo.getPamt()));
+        dto.setTime(Float.parseFloat(vo.getTime()));
+        dto.setRate(Float.parseFloat(vo.getRate()));
+
+        // Use service to calculate simple interest
+        String result = service.calculateSimpleIntrest(dto);
+        return result;
+    }
 }
