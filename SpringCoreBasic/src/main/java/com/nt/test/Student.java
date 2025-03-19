3d23ceec781a3e4d7bc77a1ac4895205afbefd84
Pradip Ghadge <121886588+GhadgePradip9971@users.noreply.github.com>
2025-03-19 19:01:30 +0530

    Spring core Basic Proj

---
+++ SpringCoreBasic/src/main/java/com/nt/test/Student.java
@@ -0,0 +1,17 @@
+package com.nt.test;
+
+public class Student {
+	/*
+	 * public Student(String name) { super(); Name = name; }
+	 */
+
+private String Name;
+
+public String getName() {
+	return Name;
+}
+
+public void setName(String name) {
+	Name = name;
+}
+}
